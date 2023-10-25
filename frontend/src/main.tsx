05190bb7af6c4c50552c3a90ae269f7761e3e6ac
Jonas <115617616+bojonas@users.noreply.github.com>
2023-10-25 17:12:23 +0200

    Add files via upload

---
+++ frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/home.tsx";
+import { RegisterPage } from "./pages/register.tsx";
+import { LoginPage } from "./pages/login.tsx";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/home" element={<HomePage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+)
